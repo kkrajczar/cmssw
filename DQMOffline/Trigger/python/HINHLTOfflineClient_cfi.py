3c609df86dad6f0c0960873c93a541faa835f1c0
Krajczar <krisztian.krajczar@cern.ch>
2015-02-12 18:30:44 +0100

    efficiency calculation for a jet path added

--- DQMOffline/Trigger/python/HINHLTOfflineClient_cfi.py
+++ DQMOffline/Trigger/python/HINHLTOfflineClient_cfi.py
@@ -1,13 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 hinClient = cms.EDAnalyzer("DQMGenericClient",
-    subDirs        = cms.untracked.vstring("HLT/HIN/HLT_HIHFCoinc*"),
+    subDirs        = cms.untracked.vstring("HLT/HIN/HLT_HIHFCoinc*",\
+                                           "HLT/HIN/HLT_PuAK4CaloJet40_v*"),
     verbose        = cms.untracked.uint32(0), # Set to 2 for all messages
     outputFileName = cms.untracked.string(''),
     commands       = cms.vstring(),
     resolution     = cms.vstring(),
     efficiency     = cms.vstring(
-        "effTESTHF 'Testing things' FromReco_Eff_nominator FromReco_Eff_denominator",
+        "HFEffTEST 'Efficiency' FromReco_Eff_nominator FromReco_Eff_denominator",
+        "EffVsLeading 'Efficiency' recoCaloJets_pt recoCaloJets_Eff_denominator_pt"
     ),
 )
 
