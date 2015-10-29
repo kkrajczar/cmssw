9fda328ad70490cbeaf566a7337ecc5c4ffa1960
Krajczar <krisztian.krajczar@cern.ch>
2015-10-29 20:34:53 +0100

    added vtx step

--- HeavyIonsAnalysis/Configuration/python/HI_RAASkim_cff.py
+++ HeavyIonsAnalysis/Configuration/python/HI_RAASkim_cff.py
@@ -3,10 +3,34 @@ import FWCore.ParameterSet.Config as cms
 # HLT jet trigger
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 hltHIJet150 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-hltHIJet150.HLTPaths = ["HLT_PuAK4CaloJet150*","HLT_AK4CaloJet150_*"]
+hltHIJet150.HLTPaths = ["HLT_PuAK4CaloJet150*"]
 hltHIJet150.throw = False
 hltHIJet150.andOr = True
 
+hltJet150 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+hltJet150.HLTPaths = ["HLT_AK4CaloJet150_*"]
+hltJet150.throw = False
+hltJet150.andOr = True
+
+# selection of valid vertex
+primaryHIVertexFilterForJets = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("hiSelectedVertex"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2"), 
+    filter = cms.bool(True),   # otherwise it won't filter the events
+    )
+
+primaryPPVertexFilterForJets = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVerticesWithBS"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2"), 
+    filter = cms.bool(True),   # otherwise it won't filter the events
+    )
+
 HIJet150SkimSequence = cms.Sequence(
+        primaryHIVertexFilterForJets *
 	hltHIJet150
+)
+
+Jet150SkimSequence = cms.Sequence(
+        primaryPPVertexFilterForJets *
+	hltJet150
 )
