2887ed603ce41f822209cc43f150c1967a24b547
Krajczar <krisztian.krajczar@cern.ch>
2015-10-29 11:17:30 +0100

    change the name of the trigger, remove unnecessary eventContent

--- HeavyIonsAnalysis/Configuration/python/HI_RAASkim_cff.py
+++ HeavyIonsAnalysis/Configuration/python/HI_RAASkim_cff.py
@@ -3,40 +3,10 @@ import FWCore.ParameterSet.Config as cms
 # HLT jet trigger
 import HLTrigger.HLTfilters.hltHighLevel_cfi
 hltHIJet150 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-hltHIJet150.HLTPaths = ["HLT_HIJet150*","HLT_Jet150*"]
+hltHIJet150.HLTPaths = ["HLT_PuAK4CaloJet150*","HLT_AK4CaloJet150_*"]
 hltHIJet150.throw = False
 hltHIJet150.andOr = True
 
 HIJet150SkimSequence = cms.Sequence(
 	hltHIJet150
-)
-
-#event content
-hltHIJet150_EventContent = cms.PSet(
-	outputCommands = cms.untracked.vstring(
-		'drop *',
-		#------- RAW content ------
-		'keep L1GlobalTriggerObjectMapRecord_*_*',
-		'keep edmTriggerResults_*_*',
-		'keep triggerTriggerEvent_*_*',
-		'keep FEDRawDataCollection_*_*',
-		#------- CaloJet collections ------
-		'keep recoCaloJets_ak2CaloJets_*_*',
-		'keep recoCaloJets_ak3CaloJets_*_*',
-		'keep recoCaloJets_ak4CaloJets_*_*',
-		'keep recoCaloJets_ak5CaloJets_*_*',
-		'keep recoCaloJets_ak6CaloJets_*_*',
-		'keep recoCaloJets_akPu2CaloJets_*_*',
-		'keep recoCaloJets_akPu3CaloJets_*_*',
-		'keep recoCaloJets_akPu4CaloJets_*_*',
-		'keep recoCaloJets_akPu5CaloJets_*_*',
-		'keep recoCaloJets_akPu6CaloJets_*_*',
-		#------- Tracks collection --------
-		'keep recoTracks_generalTracks_*_*',
-		#------- CaloTower collection -----
-		'keep *_towerMaker_*_*',
-		#------- Various collections ------
-		'keep *_EventAuxilary_*_*',
-		'keep *_offlinePrimaryVertices_*_*',
-		'keep *_hcalnoise_*_*')
 )
